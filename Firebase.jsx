72d96e6966458d2ad5beafcf06f176d2278d95b7
MuzdalfaZulfiqar <Muzdalfazulfiqar11@gmail.com>
2024-08-01 20:26:02 +0500

    first commit

---
+++ Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2l-ljSm_6QMphj4iZxSHMMuSOlr8mn3w",
+  authDomain: "notes-b9244.firebaseapp.com",
+  projectId: "notes-b9244",
+  storageBucket: "notes-b9244.appspot.com",
+  messagingSenderId: "105499230959",
+  appId: "1:105499230959:web:818862f9412ad5a789111a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
